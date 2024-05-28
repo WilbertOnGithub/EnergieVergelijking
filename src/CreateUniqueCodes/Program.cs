a9763577e094d77ae5b182eebf7e4b9d5f273709
Wilbert van Dolleweerd <wilbert@arentheym.com>
2024-05-28 11:58:45 +0200

    Create program for unique codes

--- src/CreateUniqueCodes/Program.cs
+++ src/CreateUniqueCodes/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace CreateUniqueCodes;
+﻿using Sqids;
+
+namespace CreateUniqueCodes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var sqids = new SqidsEncoder<int>(new SqidsOptions
+        {
+            Alphabet = "xakyd4ejhmv5l1uf9gzo2qicp07n3wr86stb",
+            MinLength = 6
+        });
+
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(@"C:\temp", "MailMergeCodes.txt")))
+        {
+            for (int i = 0; i < 400; i++)
+            {
+                var id = sqids.Encode(i);
+                outputFile.WriteLine($"{i};{id}");
+            }
+        }
+
+        Console.WriteLine("Done");
     }
 }
