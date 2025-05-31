69b9adb4b353687cf7d166ef10d3c2589a37d2dd
Wilbert van Dolleweerd <wilbert@arentheym.com>
2025-05-31 15:33:56 +0200

    Configure warnings

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace Presentation;
 
-public class Program
+internal static class Program
 {
     public static async Task Main(string[] args)
     {
