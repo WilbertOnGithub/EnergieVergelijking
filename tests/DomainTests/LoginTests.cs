0bf82d38b0a9a5b1b68e4e37df7f94fd302d3222
Wilbert van Dolleweerd <wilbert@arentheym.com>
2025-05-31 13:02:07 +0200

    Add tests for logging in

---
+++ tests/DomainTests/LoginTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Arentheym.EnergieVergelijker.Domain;
+using FluentAssertions;
+using Xunit;
+
+namespace DomainTests;
+
+[SuppressMessage(
+    "Naming",
+    "CA1707:Identifiers should not contain underscores",
+    Justification = "Unit test method naming"
+)]
+public class LoginTests
+{
+    [Fact]
+    public void Using_Unknown_Code_Returns_False()
+    {
+        // Arrange
+        var vergelijker = new Vergelijker();
+
+        // ACT
+        bool result = vergelijker.CodeExists(Guid.NewGuid().ToString());
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Using_Known_Code_Returns_True()
+    {
+        // Arrange
+        var vergelijker = new Vergelijker();
+
+        // ACT
+        bool result = vergelijker.CodeExists("n5lvbv");
+
+        // Assert
+        result.Should().BeTrue();
+    }
+}
