6fbeb0d515f0f17b3aa1838d8340c716c63d6edd
Wilbert van Dolleweerd <wilbert@arentheym.com>
2025-05-31 12:16:17 +0200

    Add ApplicationTests skeleton

---
+++ tests/ApplicationTests/MapperTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+
+using FluentAssertions;
+using Xunit;
+
+namespace ApplicationTests;
+
+[SuppressMessage(
+    "Naming",
+    "CA1707:Identifiers should not contain underscores",
+    Justification = "Unit test method naming"
+)]
+public class MapperTests
+{
+    private readonly IFixture _fixture;
+
+    private MapperTests()
+    {
+        _fixture = new Fixture().Customize(new AutoNSubstituteCustomization());
+    }
+
+    [Fact]
+    public void SearchFilterDto_To_SearchFilter_Should_Map_Correctly()
+    {
+
+    }
+}
