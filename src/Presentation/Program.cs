d3e941625ffbc4d61f027a2d62d771e9c53a17bd
Wilbert van Dolleweerd <wilbert@arentheym.com>
2025-06-20 19:55:44 +0200

    Add example of energy usage charts

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -1,3 +1,5 @@
+using ApexCharts;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,6 +13,7 @@ internal static class Program
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
+        builder.Services.AddApexCharts();
         builder.Services.AddSingleton<Application.Login>();
         builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
