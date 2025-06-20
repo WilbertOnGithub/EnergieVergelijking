fcac627a1c8b795526495e0dc3b929f7bb0cb182
Wilbert van Dolleweerd <wilbert@arentheym.com>
2025-06-20 17:09:21 +0200

    Create skeleton login page

---
+++ src/Presentation/Pages/Login.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Presentation.Pages;
+
+#pragma warning disable CA1515
+public partial class Login : ComponentBase
+#pragma warning restore CA1515
+{
+    public string UniqueCode { get; set; } = string.Empty;
+
+    private void HandleSubmit()
+    {
+        Console.WriteLine($"Received text input: {UniqueCode}");
+    }
+}
