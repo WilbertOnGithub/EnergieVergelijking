88eca46711d800242b5e0eb10291063e81e14dc9
Wilbert van Dolleweerd <wilbert@arentheym.com>
2025-06-20 18:08:14 +0200

    Set CA1515 to suppress

--- src/Presentation/Pages/Login.razor.cs
+++ src/Presentation/Pages/Login.razor.cs
@@ -2,9 +2,7 @@
 
 namespace Presentation.Pages;
 
-#pragma warning disable CA1515
 public partial class Login : ComponentBase
-#pragma warning restore CA1515
 {
     public string UniqueCode { get; set; } = string.Empty;
 
