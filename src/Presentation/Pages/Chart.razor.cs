30c181e961d492dc2b4a703cec8e76ef3a2fbb44
Wilbert van Dolleweerd <wilbert@arentheym.com>
2025-06-27 16:29:37 +0200

    Average now working

--- src/Presentation/Pages/Chart.razor.cs
+++ src/Presentation/Pages/Chart.razor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 using ApexCharts;
 
@@ -10,6 +11,10 @@ namespace Arentheym.EnergieVergelijker.Presentation.Pages;
 
 public partial class Chart(Searcher searcher) : ComponentBase
 {
+    private const string PowerColor = "orange";
+    private const string GasColor = "darkblue";
+    private readonly CultureInfo dutchCulture = new("en-NL");
+
     private ApexChart<ClusterWoningDto>? chart;
     private readonly ApexChartOptions<ClusterWoningDto> options = new();
 
@@ -20,15 +25,51 @@ public partial class Chart(Searcher searcher) : ComponentBase
 
     protected override void OnInitialized()
     {
-        options.Colors = ["lightgray", "lightblue"];
+        options.Colors = [PowerColor, GasColor];
     }
 
     protected override async Task OnParametersSetAsync()
     {
         Woningen = searcher.Search(FilterSelection);
+        var averageKwh = Woningen.Any() ? (decimal)Woningen.Average(w => w.KiloWattUur) : 0;
+        var averageGasUsage = Woningen.Any() ? (decimal)Woningen.Average(w => w.KubiekeMeterGas) : 0;
+
+        options.Annotations = new Annotations
+        {
+            Yaxis =
+            [
+                new AnnotationsYAxis
+                {
+                    Y = averageKwh,
+                    BorderWidth = 2,
+                    BorderColor = PowerColor,
+                    StrokeDashArray = 2,
+                    Label = new Label
+                    {
+                        BorderColor = PowerColor,
+                        Style = new Style {Color = "black", Background = PowerColor},
+                        Text = $"Gemiddeld stroomverbruik: {averageKwh.ToString("N0", dutchCulture)} kWh"
+                    }
+                },
+                new AnnotationsYAxis
+                {
+                    Y = averageGasUsage,
+                    BorderColor = GasColor,
+                    StrokeDashArray = 2,
+                    Label = new Label
+                    {
+                        BorderColor = GasColor,
+                        Style = new Style {Color = "white", Background = GasColor},
+                        Text = $"Gemiddeld gasverbruik: {averageGasUsage.ToString("N0", dutchCulture)} m2"
+                    }
+                }
+            ]
+        };
+
         if (chart != null)
         {
             await chart.UpdateSeriesAsync();
+            await chart.UpdateOptionsAsync(redrawPaths: true, animate: true, updateSyncedCharts: true);
         }
     }
 }
