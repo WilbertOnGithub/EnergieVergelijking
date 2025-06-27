97829338f36e56359e99158ca8ca7119833c594b
Wilbert van Dolleweerd <wilbert@arentheym.com>
2025-06-27 15:26:30 +0200

    Add colors for series

--- src/Presentation/Pages/Chart.razor.cs
+++ src/Presentation/Pages/Chart.razor.cs
@@ -11,16 +11,16 @@ namespace Arentheym.EnergieVergelijker.Presentation.Pages;
 public partial class Chart(Searcher searcher) : ComponentBase
 {
     private ApexChart<ClusterWoningDto>? chart;
+    private readonly ApexChartOptions<ClusterWoningDto> options = new();
 
     private IReadOnlyList<ClusterWoningDto> Woningen { get; set; } = [];
 
     [Parameter]
     public SearchFilterDto FilterSelection { get; set; } = new();
 
-    protected override async Task OnInitializedAsync()
+    protected override void OnInitialized()
     {
-        Woningen = searcher.Search(FilterSelection);
-        await Task.CompletedTask;
+        options.Colors = ["lightgray", "lightblue"];
     }
 
     protected override async Task OnParametersSetAsync()
