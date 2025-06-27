675173ab72b4516be132ab2eba1d8c668826fa49
Wilbert van Dolleweerd <wilbert@arentheym.com>
2025-06-27 12:13:18 +0200

    Set the filter for the chart

--- src/Presentation/Pages/Chart.razor.cs
+++ src/Presentation/Pages/Chart.razor.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Arentheym.EnergieVergelijker.Application;
+
+using Microsoft.AspNetCore.Components;
 
 namespace Arentheym.EnergieVergelijker.Presentation.Pages;
 
 public partial class Chart : ComponentBase
 {
+    [Parameter]
+    public SearchFilterDto FilterSelection { get; set; } = new();
 }
--- src/Presentation/Pages/Filter.razor.cs
+++ src/Presentation/Pages/Filter.razor.cs
@@ -6,18 +6,20 @@ namespace Arentheym.EnergieVergelijker.Presentation.Pages;
 
 public partial class Filter : ComponentBase
 {
-    private WoningTypeDto? SelectedWoningType { get; set; }
-
-    private AantalWoonlagenDto? SelectedAantalWoonlagen { get; set; }
-
-    private GebruikOpenHaardDto? SelectedGebruikOpenHaard { get; set; }
-
-    private GezinssituatieDto? SelectedGezinsSituatie { get; set; }
-
     private IsolatieMaatregelenDto SelectedIsolatieMaatregelen { get; set; }
+
+    private SearchFilterDto SelectedFilterDto { get; set; } = new();
 
     private void ToggleIsolatieMaatregel(IsolatieMaatregelenDto maatregel)
     {
-        SelectedIsolatieMaatregelen ^= maatregel;
+        // Start with the current selection, or 'Geen' if null
+        var currentSelection = SelectedFilterDto.IsolatieMaatregelen ?? IsolatieMaatregelenDto.Geen;
+
+        // Toggle the selected measure
+        currentSelection ^= maatregel;
+
+        // If the result is 'Geen', set the DTO property to null to indicate no filter is applied.
+        // Otherwise, update it with the new combined value.
+        SelectedFilterDto.IsolatieMaatregelen = currentSelection == IsolatieMaatregelenDto.Geen ? null : currentSelection;
     }
 }
